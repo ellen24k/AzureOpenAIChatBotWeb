ce36b2c8107bf6a9572a8e9637372325767197bf
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 08:50:07 +0900

    login

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 import os
-
 import streamlit as st
-# import asyncio
-# from pkg_azure_ai import chatbot
-# from pkg_azure_ai.dalle import generate_image
-from pkg_db.db import DatabaseManager
-# from pkg_utils.utils import make_history
-from login import login_page
+from langchain.llms import AzureOpenAI
+from langchain.prompts import PromptTemplate
 
+# Azure OpenAI 설정
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 
+llm = AzureOpenAI(api_key=api_key, endpoint=endpoint)
 
 def app_page():
-    # Streamlit 애플리케이션 설정
     st.title('3행시 갤러리 using TTS, STT, Dall-E3, Chat-GPT4o with VTube')
-
-    # 데이터베이스 초기화
-    db = DatabaseManager()
-
-    # 사용자 입력 받기
     user_input = st.text_input('3행시를 만들 3글자를 입력하세요:')
 
     if st.button('시작'):
@@ -25,49 +18,12 @@ def app_page():
             st.write(f"[사용자]\n{user_input}\n")
             st.write('인공지능이 단어를 가지고 삼행시를 생성 중 입니다.')
 
-            # 챗봇 응답 생성
-            # generated_text = chatbot.chat(user_input)
-            # st.write(f"[챗봇]\n{generated_text}\n\n")
+            # Azure OpenAI에 질의
+            prompt = PromptTemplate(template="Create a poem with the following three characters: {input}", input_variables=["input"])
+            query = prompt.format(input=user_input)
+            response = llm(query)
 
-            # 이미지 생성
-            # img_url = asyncio.run(generate_image(generated_text))
+            st.write(f"[챗봇]\n{response}\n\n")
 
-            # if img_url is not None:
-            # now = make_history(generated_text, img_url, f'temp/gen_text.wav')
-            # else:
-            #     st.write("부적절한 단어 사용등의 이유로 이미지 생성이 취소되었습니다. 기본이미지로 저장합니다.")
-            # now = make_history(generated_text, 'default_img', f'temp/gen_text.wav')
-
-            # 데이터베이스에 저장
-            # db.add_data(now, user_input)
-
-            # 생성된 이미지 표시
-            # st.image(img_url if img_url else 'default_img', caption='Generated Image')
-
-    # 히스토리 표시
-    st.write('히스토리')
-    all_data = db.select_all_data()
-    for row in all_data:
-        st.write(row[1])
-
-    for row in all_data:
-        file_id = row[0]
-        title = row[1]
-        st.write(f"Title: {title}")
-
-        # 이미지 표시
-        img_path = f'history/{file_id}.png'
-        if os.path.exists(img_path):
-            print(img_path)
-            # st.image(img_path, caption=title)
-
-        # 버튼을 클릭하면 wav 파일 재생
-        if st.button(f"Play {title}"):
-            wav_path = f'history/{file_id}.wav'
-            if os.path.exists(wav_path):
-                audio_file = open(wav_path, 'rb')
-                audio_bytes = audio_file.read()
-                st.audio(audio_bytes, format='audio/wav')
-            else:
-                st.write("WAV 파일을 찾을 수 없습니다.")
-
+if __name__ == '__main__':
+    app_page()
