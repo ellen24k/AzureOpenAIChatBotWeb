5adac6c7ee506b3727af4f4d07582f061aa15549
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 00:10:50 +0900

    login

--- main_web.py
+++ main_web.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 
 # 사용자 데이터베이스 (예시)
@@ -15,6 +14,7 @@ if 'logged_in' not in st.session_state:
 def login(username, password):
     if username in USER_DATA and USER_DATA[username] == password:
         st.session_state['logged_in'] = True
+        st.experimental_set_query_params(page="main_app")
         st.success("로그인 성공!")
     else:
         st.error("로그인 실패. 사용자 이름 또는 비밀번호를 확인하세요.")
@@ -22,11 +22,16 @@ def login(username, password):
 # 로그아웃 함수
 def logout():
     st.session_state['logged_in'] = False
+    st.experimental_set_query_params(page="login")
     st.success("로그아웃 되었습니다.")
 
-# 로그인 상태에 따른 화면 표시
-if st.session_state['logged_in']:
-    st.write("환영합니다!")
+# URL 매개변수 확인
+query_params = st.experimental_get_query_params()
+page = query_params.get("page", ["login"])[0]
+
+# 페이지 전환
+if page == "main_app" and st.session_state['logged_in']:
+    st.write("환영합니다! 메인 앱 페이지입니다.")
     if st.button("로그아웃"):
         logout()
 else:
