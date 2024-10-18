3542cc914315ca500a6a697c215d84e722514d81
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 21:10:35 +0900

    마이크

--- pkg_utils/mic.py
+++ pkg_utils/mic.py
@@ -1,21 +1,25 @@
-# Add this import at the top of `views/home.py`
 import streamlit as st
-import speech_recognition as sr
+import azure.cognitiveservices.speech as speechsdk
 
-def capture_audio():
-    recognizer = sr.Recognizer()
-    mic = sr.Microphone()
+speech_key = st.secrets["SPEECH_KEY"]
+service_region = st.secrets["SERVICE_REGION"]
 
-    with mic as source:
-        st.info("마이크를 통해 음성을 입력하세요...")
-        audio = recognizer.listen(source)
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+audio_config = speechsdk.AudioConfig(use_default_microphone=True)
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config, language="ko-KR")
 
-    try:
-        text = recognizer.recognize_google(audio, language="ko-KR")
-        st.success(f"인식된 텍스트: {text}")
-        return text
-    except sr.UnknownValueError:
-        st.error("음성을 인식할 수 없습니다.")
-    except sr.RequestError as e:
-        st.error(f"음성 인식 서비스에 접근할 수 없습니다: {e}")
+def recognize_speech():
+    result = speech_recognizer.recognize_once()
+
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(result.text))
+        return result.text
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(result.no_match_details))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+    return None
 
--- pkg_views/home.py
+++ pkg_views/home.py
@@ -2,23 +2,32 @@ import streamlit as st
 
 from langchain_openai import AzureChatOpenAI
 
-from pkg_utils.mic import capture_audio
+from pkg_utils.mic import recognize_speech
 
+def make_poem(user_input):
+    chat_model = AzureChatOpenAI()
+    st.write(f"[사용자]\n")
+    st.write(f"{user_input}\n\n")
+    result = chat_model.invoke(input=user_input + '의 3글자로 3행시 만들어. 형식은 세줄로 "글자: 내용" 만 작성해')
+    st.write(f"[챗봇]\n")
+    st.write(f"{result.content}\n\n")
 
 def load_view():
-    chat_model = AzureChatOpenAI()
-
     st.title('3행시 갤러리 using TTS, STT, Dall-E3, Chat-GPT4o with VTube, Streamlit, Supabase, Azure.')
-    user_input = st.text_input('3행시를 만들 3글자를 입력하세요:')
-
-    if st.button('시작'):
-        with st.spinner('인공지능이 단어를 가지고 삼행시를 생성 중 입니다.'):
-            if user_input:
-                st.write(f"[사용자]\n")
-                st.write(f"{user_input}\n\n")
-                result = chat_model.invoke(input=user_input + '의 3글자로 3행시 만들어. 형식은 세줄로 "글자: 내용" 만 작성해')
-                st.write(f"[챗봇]\n")
-                st.write(f"{result.content}\n\n")
-
+    user_input = None
     if st.button('마이크로 입력'):
-        user_input = capture_audio()
+        with st.spinner('마이크로 입력 중...'):
+            user_input = recognize_speech()
+        if not user_input:
+            user_input = st.text_input('직접 3행시를 만들 3글자를 입력하세요:')
+            if st.button('3행시 만들기'):
+                with st.spinner('인공지능이 단어를 가지고 삼행시를 생성 중 입니다.'):
+                    make_poem(user_input)
+        else:
+            st.write(user_input + ' 단어가 인식되었습니다.')
+            with st.spinner('인공지능이 단어를 가지고 삼행시를 생성 중 입니다.'):
+                make_poem(user_input)
+
+
+
+
