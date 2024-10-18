f822a4a84b0c9987cbb611a7f40400c3e1a7086a
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 14:30:48 +0900

    상단메뉴 chat

--- main_web.py
+++ main_web.py
@@ -4,39 +4,6 @@ import streamlit as st
 from app import app_page
 from login import login_page
 
-from streamlit_navigation_bar import st_navbar
-
-def menu():
-    st.set_page_config(initial_sidebar_state="collapsed")
-
-    pages = ["Home", "Library", "Tutorials", "Development", "Download"]
-    styles = {
-        "nav": {
-            "background-color": "rgb(123, 209, 146)",
-        },
-        "div": {
-            "max-width": "32rem",
-        },
-        "span": {
-            "border-radius": "0.5rem",
-            "color": "rgb(49, 51, 63)",
-            "margin": "0 0.125rem",
-            "padding": "0.4375rem 0.625rem",
-        },
-        "active": {
-            "background-color": "rgba(255, 255, 255, 0.25)",
-        },
-        "hover": {
-            "background-color": "rgba(255, 255, 255, 0.35)",
-        },
-    }
-
-    page = st_navbar(pages, styles=styles)
-    st.write(page)
-
-    with st.sidebar:
-        st.write("Sidebar")
-
 
 def main():
     if 'logged_in' not in st.session_state:
@@ -44,7 +11,6 @@ def main():
 
     print(st.session_state['logged_in'])
     if st.session_state['logged_in']:
-        menu()
         app_page()
     else:
         login_page()
