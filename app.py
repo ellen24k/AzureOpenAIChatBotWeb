65f5b81eba57120c5fa5e1b5d63c0c4394bfb141
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 09:01:21 +0900

    login

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 import os
 import streamlit as st
 from langchain.llms import AzureOpenAI
-from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
 
-# Azure OpenAI 설정
-api_key = os.getenv("AZURE_OPENAI_API_KEY")
-endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 
-llm = AzureOpenAI(api_key=api_key, endpoint=endpoint)
+
 
 def app_page():
+    chat_model = ChatOpenAI()
+
     st.title('3행시 갤러리 using TTS, STT, Dall-E3, Chat-GPT4o with VTube')
     user_input = st.text_input('3행시를 만들 3글자를 입력하세요:')
 
+
     if st.button('시작'):
-        if user_input:
-            st.write(f"[사용자]\n{user_input}\n")
-            st.write('인공지능이 단어를 가지고 삼행시를 생성 중 입니다.')
+        with st.spinner('인공지능이 단어를 가지고 삼행시를 생성 중 입니다.'):
+            if user_input:
+                st.write(f"[사용자]\n{user_input}\n")
+                result = chat_model.predict(user_input + '의 3글자로 3행시 만들어. 형식은 "글자: 내용," 만 작성해')
 
-            # Azure OpenAI에 질의
-            prompt = PromptTemplate(template="Create a poem with the following three characters: {input}", input_variables=["input"])
-            query = prompt.format(input=user_input)
-            response = llm(query)
+                # prompt = PromptTemplate(template="Create a poem with the following three characters: {input}", input_variables=["input"])
+                # query = prompt.format(input=user_input)
+                # response = llm(query)
 
-            st.write(f"[챗봇]\n{response}\n\n")
+                st.write(f"[챗봇]\n{result}\n\n")
 
 if __name__ == '__main__':
     app_page()
