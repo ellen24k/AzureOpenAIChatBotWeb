5ce66974757b37058ca2c5b3c0ab011d76f455b1
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 14:11:27 +0900

    상단메뉴 chat

--- main_web.py
+++ main_web.py
@@ -5,17 +5,11 @@ from app import app_page
 from login import login_page
 
 # 페이지 구성 설정
-st.set_page_config(page_title="My App", layout="wide")
-
-# CSS를 사용하여 Share, Fork, Edit 버튼 숨기기
-st.markdown(
-    """
-    <style>
-    .stApp .header {display: none;}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# st.set_page_config(
+#     page_title="My App",
+#     page_icon="🏠",
+#     layout="wide"
+# )
 
 def main():
     if 'logged_in' not in st.session_state:
