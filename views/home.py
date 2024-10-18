f558bca462f01706ebe10be7315e7e0d50d3d878
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 20:11:16 +0900

    상단메뉴 chat

--- views/home.py
+++ views/home.py
@@ -5,7 +5,7 @@ from langchain_openai import AzureChatOpenAI
 def load_view():
     chat_model = AzureChatOpenAI()
 
-    st.title('3행시 갤러리 using TTS, STT, Dall-E3, Chat-GPT4o with VTube, Streamlit, Supabase, Azure')
+    st.title('3행시 갤러리 using TTS, STT, Dall-E3, Chat-GPT4o with VTube, Streamlit, Supabase, Azure.')
     user_input = st.text_input('3행시를 만들 3글자를 입력하세요:')
 
     if st.button('시작'):
