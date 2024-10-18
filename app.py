0c7b4ad4e535751ab0ad6fc21d69db174d8dd402
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-18 14:37:08 +0900

    상단메뉴 chat

--- app.py
+++ app.py
@@ -1,42 +1,8 @@
 import streamlit as st
 
 from langchain.chat_models import AzureChatOpenAI
-from streamlit_navigation_bar import st_navbar
-
-
-def menu():
-    st.set_page_config(initial_sidebar_state="collapsed")
-
-    pages = ["Home", "Library", "Tutorials", "Development", "Download"]
-    styles = {
-        "nav": {
-            "background-color": "rgb(123, 209, 146)",
-        },
-        "div": {
-            "max-width": "32rem",
-        },
-        "span": {
-            "border-radius": "0.5rem",
-            "color": "rgb(49, 51, 63)",
-            "margin": "0 0.125rem",
-            "padding": "0.4375rem 0.625rem",
-        },
-        "active": {
-            "background-color": "rgba(255, 255, 255, 0.25)",
-        },
-        "hover": {
-            "background-color": "rgba(255, 255, 255, 0.35)",
-        },
-    }
-
-    page = st_navbar(pages, styles=styles)
-    st.write(page)
-
-    with st.sidebar:
-        st.write("Sidebar")
 
 def app_page():
-    menu()
     chat_model = AzureChatOpenAI()
 
     st.title('3행시 갤러리 using TTS, STT, Dall-E3, Chat-GPT4o with VTube')
