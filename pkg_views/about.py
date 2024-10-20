636553f14b6e1a289d2442aba70338fc570f3f35
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-20 13:51:58 +0900

    feat(pkg_views/home): 3행시 생성 후 이미지와 오디오 출력, 데이타베이스에 저장
    - 3행시 생성 후 Dall-e3로 이미지를 생성하고, 오디오를 생성합니다.
    - 생성된 이미지와 오디오를 출력합니다.
    - 생성된 이미지와 오디오를 데이타베이스에 저장합니다.

--- pkg_views/about.py
+++ pkg_views/about.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 def load_view():
+
     st.title('3행시 갤러리 using STT, TTS, Dall-e3, Chat-GPT4o with LangChain, GitHub, Streamlit, Supabase, Azure Cloud.')
     st.image("https://raw.githubusercontent.com/ellen24k/AzureOpenAIChatBotWeb/main/resources/diagram.png", use_column_width=True)
