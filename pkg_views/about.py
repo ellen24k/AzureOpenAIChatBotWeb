7d89b398cd5a9a6ef02aa60e049032c11b88b8a2
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-22 03:06:55 +0900

    다이어그램 변경

--- pkg_views/about.py
+++ pkg_views/about.py
@@ -5,7 +5,7 @@ from pkg_utils.utils import padding_set
 
 def load_view():
     padding_set()
-    # st.title('삼행시 갤러리 using STT, TTS, Dall-e3, Chat-GPT4o with LangChain, GitHub, Streamlit, Supabase Storage, PostreSQL, Azure AI, Azure Speech.')
+    # st.title('삼행시 갤러리 using TTS, Dall-e3, Chat-GPT4o with LangChain, GitHub, Streamlit, Supabase Storage, PostreSQL, Azure AI, Azure Speech.')
     st.title('삼행시 갤러리 using Dall-e, Chat-GPT4o')
     # st.image("https://raw.githubusercontent.com/ellen24k/AzureOpenAIChatBotWeb/main/resources/diagram.png", use_column_width=True)
     st.write('만든이 : 김태영')
