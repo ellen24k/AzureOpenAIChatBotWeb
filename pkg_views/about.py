d4ff384efe83828560f58d66537367c20e96d4d6
TaeYoung Kim <ellen24ks@gmail.com>
2024-10-23 03:25:44 +0900

    정리

--- pkg_views/about.py
+++ pkg_views/about.py
@@ -7,6 +7,8 @@ def load_view():
     padding_set()
     # st.title('삼행시 갤러리 using TTS, Dall-e3, Chat-GPT4o with LangChain, GitHub, Streamlit, Supabase Storage, PostreSQL, Azure AI, Azure Speech.')
     st.title('삼행시 갤러리 using Dall-e, Chat-GPT4o')
-    # st.image("https://raw.githubusercontent.com/ellen24k/AzureOpenAIChatBotWeb/main/resources/diagram.png", use_column_width=True)
     st.write('만든이 : 김태영')
-    st.write(f'프로그램 버전: 1.0.1')
+    if st.button('프로그램 버전: 1.0.1'):
+        st.image("resources/diagram.png", use_column_width=True)
+
+
